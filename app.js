9e13c7af08d3114af7a252e26bbf5655c8b59ba0
Muneeb890 <138367529+Muneeb890@users.noreply.github.com>
2023-10-13 11:27:43 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,101 @@
+
+//CHAPTER NO 01
+
+//Qs no 01:
+//alert("Hello")
+
+//Qs no 02:
+//alert("Error! Please Enter a valid password")
+
+//Qs no 03:
+//alert("Welcome to JS land...\nHappy Coding!")
+
+//Qs no 04:
+//alert("Welcome to JS land...")
+//alert("Happy Coding!")
+
+//Qs n0 05:
+//console.log("Hello i can run JS through my web browser console")
+
+//CHAPTER N0 02
+
+//Qs no 01:
+//var username;
+
+//Qs no 02:
+//var myName = "Muneeb";
+
+//Qs no 03:
+//var message;
+//message= "Hello World";
+//alert(message);
+
+//Qs no 04:
+//var studentName = "Muhammad Muneeb";
+//var studentAge = "21 Years Old";
+//var courseEnrolled = "Web and App Development";
+//alert(studentName);
+//alert(studentAge);
+//alert(courseEnrolled);
+
+//Qs no 05:
+//alert("PIZZA\nPIZZ\nPIZ\nPI\nP")
+
+//Qs no 06:
+//var email = "m.muneebuddin890@gmail.com";
+//var message = "My email address is " + email;
+//alert(message);
+
+//Qs no 07:
+//var book = "A smarter \nway to learn JavaScript";
+//var message = "I am currently reading the book " + book;
+//alert(message);
+
+//Qs no 08:
+//var text = "Yah! I can write HTML content through JavaScript";
+//document.write(text)
+
+//Qs no 09:
+//var mystring = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(mystring);
+//document.write(mystring);
+
+//CHAPTER NO 03
+//Qs no 01:
+//var age = "21 years old";
+//alert("I am " + age)
+
+//Qs no 02:
+//var count = "14 times"
+//alert("You have visited this site " + count)
+
+//Qs no 03:
+//var birthyear = 2002;
+//document.write("My birth year is " + birthyear);
+
+//Qs no 04:
+//var visitorName = "Ali Khan";
+//var productTitle = "T-shirt";
+//var quantity = 5; 
+//document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on JJ Clothing store");
+
+//CHAPTER N0 04
+//Qs no 01:
+//var variable1, variable2, variable3;
+
+//Qs no 02:
+//LEGAL VARIABLES
+//myVar
+//_value
+//$price
+//thisIsALongVariableName
+//counter123
+
+//ILLEGAL VARIABLES
+//123abc
+//my-variable 
+//alert 
+//first name 
+//@username 
+
+//Qs no 03:
